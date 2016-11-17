63dcf0cafa1d9ad4ae5ed17819d13fccb356ba22
tclyjy <330365043@qq.com>
2016-11-17 21:08:28 +0800

    http.js>>异步请求豆瓣API 自定义回调函数名称

--- http.js
+++ http.js
@@ -2,7 +2,7 @@
  * @Author: XM-web
  * @Date:   2016-11-17 19:50:09
  * @Last Modified by:   XM-web
- * @Last Modified time: 2016-11-17 20:29:19
+ * @Last Modified time: 2016-11-17 21:06:28
  */
 
 'use strict';
@@ -13,18 +13,19 @@
     angular.module('moviecat.services.http', [])
         .service('httpService', ['$document', '$window', function($document, $window) {
             this.jsonp = function(url, data, callback) {
-            
+                //自定义函数名称
                 var callbackName = 'my_jsonp_' + Math.random().toString().replace('.', '');
-                
-                console.log(callback);
+                //挂载回调函数       
                 $window[callbackName] = callback;
-                console.log($window[callbackName]);
+                //处理URL地址
                 var querystring = url.indexOf('?') == -1 ? '?' : '&';
                 for (var key in data) {
                     querystring += key + '=' + data[key] + '&';
                 }
                 querystring += 'callback=' + callbackName;
+                //创建script标签
                 var scriptElement = $document[0].createElement('script');
+                //将scrpit标签添加到页面上
                 scriptElement.src = url + querystring;
                 $document[0].body.appendChild(scriptElement);
             };
